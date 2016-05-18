36285182302f878ce936d40ca766c59e2900b1e1
Oscar F <ofalkman@gmail.com>
2016-05-18 14:01:37 +0200

    Fixed annoyance.

--- se.kth.ict.nextgenpos/view/View.java
+++ se.kth.ict.nextgenpos/view/View.java
@@ -14,26 +14,26 @@ public class View {
      * @param cont           The controller of the application.
      */
     public View(Controller cont) {
-	this.cont = cont;
-    }
-
-    /**
-     * Simulates a view. Makes some calls to the controller.
-     */
-    public void test() {
-	cont.makeNewSale();
-	enterItem(1);
-	enterItem(10);
-    }
-
-    private void enterItem(int itemId) {
-	int quantity = 1;
-	System.out.println("");
-	try {
-		System.out.println("Result for item " + itemId + ": " + cont.enterItem(itemId, quantity));
-	} catch (itemIdOutOfRangeException itemIdNotFoundException) {
-		System.out.println("There is no item with an item ID of \"" + itemIdNotFoundException.getItemId() + "\", please try again.");
-	}
-	System.out.println("");
-    }
+		this.cont = cont;
+	    }
+	
+	    /**
+	     * Simulates a view. Makes some calls to the controller.
+	     */
+	    public void test() {
+		cont.makeNewSale();
+		enterItem(1);
+		enterItem(10);
+	    }
+	
+	    private void enterItem(int itemId) {
+		int quantity = 1;
+		System.out.println("");
+		try {
+			System.out.println("Result for item " + itemId + ": " + cont.enterItem(itemId, quantity));
+		} catch (itemIdOutOfRangeException itemIdNotFoundException) {
+			System.out.println("There is no item with an item ID of \"" + itemIdNotFoundException.getItemId() + "\", please try again.");
+		}
+		System.out.println("");
+	   }
 }
